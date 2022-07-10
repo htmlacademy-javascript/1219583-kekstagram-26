825c1b3fcf0568568e3b7d83b4e4ddaaabccd8b8
Keks <keks@htmlacademy.ru>
2022-07-10 19:44:51 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/util.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-unused-expressions */
+/**
+ * Возвращает случайное число
+ * @param {number} a - минимальное значение из диапазона
+ * @param {number} b - максимальное значение из диапазона
+ * @return {integer}  - случайное число
+ */
+
+const getRandomNumber = function (a, b) {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+/**
+*Проверяет длину введенного комментария
+* @param {string} string - строка
+* @param {integer} maxLength - максимальная длина строки
+* @return {boolean}  - true/false
+*/
+
+const checkStringLength = function (string, length) {
+  return string.length <= length;
+};
+
+checkStringLength('Комментарий', 140);
+
+const isEscapeKey = (evt) => ( evt.key === 'Escape' );
+
+// const checkStringLength = (string = '', length = 0) => string.length <= length;
+
+export {getRandomNumber, isEscapeKey, checkStringLength};
