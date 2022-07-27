14846bdb8eb5962e70d97b3995c9ea9a74bbea37
Keks <keks@htmlacademy.ru>
2022-07-27 13:42:46 +0300

    :heavy_check_mark: Сборка #14

--- 14/js/api.js
+++ 14/js/api.js
@@ -13,4 +13,23 @@ const getData = (onSuccess, onFail) => {
     });
 };
 
-export { getData };
+const sendData = (onSuccess, onFail, body) => {
+  fetch('https://26.javascript.pages.academy/kekstagram',
+    {
+      method: 'POST',
+      body,
+    }
+  )
+    .then((response) => {
+      if (response.ok) {
+        onSuccess();
+      } else {
+        throw new Error('Не удалось отправить форму. Попробуйте ещё раз');
+      }
+    })
+    .catch((err) => {
+      onFail(err.message);
+    });
+};
+
+export { getData, sendData };
